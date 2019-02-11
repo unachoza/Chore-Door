a1fbb8beea019583fcad9f39ca064b671325aa9d
Arianna Choza <unachoza@gmail.com>
2019-02-11 18:55:10 -0500

    random generator

--- script.js
+++ script.js
@@ -6,12 +6,41 @@ const botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-
 const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg'
 const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg'
 
+const numClosedDoors = 3
+var openDoor1 = 0
+var openDoor2 = 0
+var openDoor3 = 0
+ 
+const randomChoreDoorGenerator= () => {
+  let choreDoor = Math.floor(Math.random() * numClosedDoors)
+  if(choreDoor === 1 ){
+    openDoor1 = botDoorPath
+    openDoor2 = beachDoorPath
+    openDoor3 = spaceDoorPath
+    console.log(botDoorPath, beachDoorPath, spaceDoorPath)
+  } else if(choreDoor === 2){
+    openDoor2 = botDoorPath
+    openDoor3 = beachDoorPath
+    openDoor1 = spaceDoorPath
+    console.log(botDoorPath, beachDoorPath, spaceDoorPath)
+  } else {
+    openDoor3 = botDoorPath
+    openDoor1 = beachDoorPath
+    openDoor2 = spaceDoorPath
+    console.log(botDoorPath, beachDoorPath, spaceDoorPath)
+  }
+  
+    
+}
+
 door1.onclick = () => {
-  doorImage1.src = botDoorPath
+  doorImage1.src = openDoor1
 }
 door2.onclick = () => {
-  doorImage2.src = beachDoorPath
+  doorImage2.src = openDoor2
 }
 door3.onclick = () => {
-  doorImage3.src = spaceDoorPath
+  doorImage3.src = openDoor3
 }
+
+randomChoreDoorGenerator()
